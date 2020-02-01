806b9041fffd7d0d60b80288fae8d7a69d272a30
alexsteeel <rf-89@list.ru>
2020-02-01 17:28:58 +0300

    Add class for receiving data from REST API.

---
+++ DataTransferFromRESTApiToDB/RESTfulAPI/RESTfulAPIDataReceiver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DataTransferFromRESTApiToDB
+{
+    /// <summary>
+    /// Получение данных из RESTful API.
+    /// </summary>
+    public class RESTfulAPIDataReceiver
+    {
+        /// <summary>
+        /// Получить данные указанного типа по ссылке.
+        /// </summary>
+        /// <typeparam name="T">Тип данных для конвертации из JSON.</typeparam>
+        /// <param name="url">Ссылка для получения данных.</param>
+        private static void Get<T>(string url)
+        {
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri(url)
+            };
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync(string.Empty).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var dataObjects = response.Content.ReadAsAsync<RootObject<T>>().Result;
+            }
+            else
+            {
+                throw new HttpRequestException($"{(int)response.StatusCode} ({response.ReasonPhrase})");
+            }
+
+            client.Dispose();
+        }
+    }
+}
---
+++ DataTransferFromRESTApiToDB/RESTfulAPI/RootObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DataTransferFromRESTApiToDB
+{
+    /// <summary>
+    /// Служебный класс для конвертации данных из JSON в модель.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RootObject<T>
+    {
+        public List<T> Data { get; set; }
+    }
+}
