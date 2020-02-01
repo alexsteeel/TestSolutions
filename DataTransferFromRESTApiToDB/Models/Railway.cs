090409bdaa6e0c8774db05efde1702330185441e
alexsteeel <rf-89@list.ru>
2020-02-01 16:35:15 +0300

    Add models Railway and Station.

---
+++ DataTransferFromRESTApiToDB/Models/Railway.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DataTransferFromRESTApiToDB
+{
+    /// <summary>
+    /// Железная дорога.
+    /// </summary>
+    public class Railway
+    {
+        /// <summary>
+        /// Идентификатор дороги (Первичный ключ).
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Код дороги.
+        /// </summary>
+        public short Code { get; set; }
+
+        /// <summary>
+        /// Наименование полное.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Наименование сокращенное.
+        /// </summary>
+        public string ShortName { get; set; }
+
+        /// <summary>
+        /// Условный идентификатор страны дороги.
+        /// </summary>
+        public int CountryID { get; set; }
+
+        /// <summary>
+        /// Короткое наименование для телеграфа.
+        /// </summary>
+        public string TelegraphName { get; set; }
+
+        /// <summary>
+        /// Дата создания записи.
+        /// </summary>
+        public DateTime DateCreate { get; set; }
+
+        /// <summary>
+        /// Дата обновления записи.
+        /// </summary>
+        public DateTime DateUpdate { get; set; }
+    }
+}
---
+++ DataTransferFromRESTApiToDB/Models/Station.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DataTransferFromRESTApiToDB.Models
+{
+    /// <summary>
+    /// Ж/д станция.
+    /// </summary>
+    public class Station
+    {
+        /// <summary>
+        /// Код станции (Первичный ключ).
+        /// </summary>
+        public int Code { get; set; }
+        
+        /// <summary>
+        /// Идентификатор станции.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Идентификатор отделения дороги.
+        /// </summary>
+        public int? RailwayDepartmentID { get; set; }
+
+        /// <summary>
+        /// Идентификатор дороги.
+        /// </summary>
+        public int? RailwayID { get; set; }
+
+        /// <summary>
+        /// Идентификатор страны.
+        /// </summary>
+        public int? CountryID { get; set; }
+
+        /// <summary>
+        /// 12-символьное наименование станции.
+        /// </summary>
+        public string Name12Char { get; set; }
+
+        /// <summary>
+        /// 40-символьное наименование станции.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Признак станции, открытой для грузовой работы.
+        /// </summary>
+        public bool FreightSign { get; set; }
+
+        /// <summary>
+        /// Код станции с контрольным знаком.
+        /// </summary>
+        public string CodeOSGD { get; set; }
+
+        /// <summary>
+        /// Дата создания записи.
+        /// </summary>
+        public DateTime DateCreate { get; set; }
+
+        /// <summary>
+        /// Дата обновления записи.
+        /// </summary>
+        public DateTime DateUpdate { get; set; }
+    }
+}
