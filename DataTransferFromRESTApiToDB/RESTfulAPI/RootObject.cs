f952ed1900cecd6e1c1b68c3161194899bfd375f
alexsteeel <rf-89@list.ru>
2020-02-02 12:29:51 +0300

    Add comments for class RootObject.

--- DataTransferFromRESTApiToDB/RESTfulAPI/RootObject.cs
+++ DataTransferFromRESTApiToDB/RESTfulAPI/RootObject.cs
@@ -5,9 +5,12 @@ namespace DataTransferFromRESTApiToDB
     /// <summary>
     /// Служебный класс для конвертации данных из JSON в модель.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">Тип данных, в который необходимо сконвертировать JSON.</typeparam>
     public class RootObject<T>
     {
+        /// <summary>
+        /// Коллекция данных.
+        /// </summary>
         public List<T> Data { get; set; }
     }
 }
